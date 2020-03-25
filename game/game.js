d77b5ca974b09311633511c8b13fcf4bb436d5c6
Patriciavaldebenito <patricia.vpanes@gmail.com>
2020-03-24 22:22:57 -0300

    Se crea array con secuencia, para eso en la clase Game se agrega método generateSequence y se accede a la variable en objeto windows

--- game/game.js
+++ game/game.js
@@ -10,17 +10,31 @@ const btnGreen = document.getElementById('btnGreen');
 class Game {
     constructor(){
         this.init();
+        this.generateSequence();
     }
 
     init(){
         btnStart.classList.add('hide');
+        this.nivel =1;
+        this.colors= {
+            btnLightBlue,
+            btnViolet,
+            btnOrange,
+            btnGreen
+        }
+    }
+
+    generateSequence(){
+        this.sequence = new Array(10).fill(0).map(n => Math.floor(Math.random()*4))
     }
 }
 
 
 function startGame(){
     console.log(`jugando init !!`);//console.log(``);
-    var game = new Game();
+    window.game = new Game(); // or console.log(game)
+   
+ 
 }
 
 startGame()
